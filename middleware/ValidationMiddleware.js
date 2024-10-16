d4fb521347cbc68df9f9e52ad0248d0bd215700e
Aditya Shukla <Shadowshukla76@gmail.com>
2024-10-16 21:57:25 +0530

    Add Validators

---
+++ middleware/ValidationMiddleware.js
@@ -0,0 +1,21 @@
+const { body, validationResult } = require('express-validator');
+
+// Middleware for validating inputs for creating or updating comics
+const validateComicInput = [
+    body('bookName').notEmpty().withMessage('Book name is required'),
+    body('authorName').notEmpty().withMessage('Author name is required'),
+    body('yearOfPublication').isInt({ min: 1900 }).withMessage('Valid publication year is required'),
+    body('price').isFloat({ min: 0 }).withMessage('Price must be a valid number'),
+    body('numberOfPages').isInt({ min: 1 }).withMessage('Number of pages must be an integer greater than 0'),
+    body('condition').isIn(['new', 'used']).withMessage('Condition must be either "new" or "used"'),
+    
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+module.exports = validateComicInput;
--- routes/ComicRoute.js
+++ routes/ComicRoute.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const {CreateComic,GetAllComics,DeleteComic,GetComic,UpdateComic}=require("../controllers/ComicController")
+const validateComicInput=require("../middleware/ValidationMiddleware")
 const router = express.Router()
 
 router.get("/", (req, res) => {
     res.send("Hii")
 })
 
-router.post("/addComic",CreateComic)
+router.post("/addComic",validateComicInput,CreateComic)
 router.get("/getComic/:id",GetComic)
 router.get("/getAllComics",GetAllComics)
 router.delete("/deleteComic/:id",DeleteComic)
-router.put("/updateComic/:id",UpdateComic)
+router.put("/updateComic/:id",validateComicInput,UpdateComic)
 
 
 module.exports=router
